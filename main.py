2293ed048cf2fee87ddfec607d115c301cf11f18
student-forlife <84071014+student-forlife@users.noreply.github.com>
2021-07-26 10:51:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from machine import ADC
 from machine import Pin
 import time
 
-# sensor connected to P16. Valid pins are P13 to P20.
+# sensor connected to P16 and P14
 # set up pin mode to input
 lightPin = Pin('P16', mode=Pin.IN)
 tempPin = Pin('P14')
 
 # create an ADC object, bits=10 means range 0-1024 the lower value the less light detected
-# adc_l = machine.ADC(bits=10)
-# adc_t = machine.ADC()
 adc = machine.ADC()
 # create an analog pin on P16;  attn=ADC.ATTN_11DB measures voltage from 0.1 to 3.3v
 lpin = adc.channel(attn=ADC.ATTN_11DB, pin=lightPin)
@@ -29,6 +27,6 @@ while True:
     
     #Sending to pybytes in channel 1
     pybytes.send_signal(1, celsius)
-    print("sending: {}".format(celsius))
+    print("Value {}".format(celsius))
     
     time.sleep(1800)  # wait 30 min
